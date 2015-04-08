1bddc8593f3431c71f7a4eed3a3cdac4bdfafff8
Dan Dzoan <dandzoan@gmail.com>
2015-04-07 21:38:25 -0700

    add composite views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require underscore
 //= require backbone
 //= require where3x3
+//= require composite_view
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
