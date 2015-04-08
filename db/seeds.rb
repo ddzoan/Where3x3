886d2f9730224165f896dd7f29785eae42c206ca
Dan Dzoan <dandzoan@gmail.com>
2015-04-08 10:41:50 -0700

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#  fname           :string           not null
+#  lname           :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string
+#  wca_id          :string
+#  delegate        :boolean          default(FALSE)
+#  description     :text
+
+dan = User.create({
+  fname: 'Dan',
+  lname: 'Dzoan',
+  email: 'ddz',
+  password: 'asdfasdf',
+  wca_id: '2006DZOA03'
+})
+ty = User.create({
+  fname: 'Tyson',
+  lname: 'Mao',
+  email: 'tmao',
+  password: 'asdfasdf',
+  wca_id: '2004MAOT02',
+
+})
+jeremy = User.create({
+  fname: 'Jeremy',
+  lname: 'Flei',
+  email: 'flei',
+  password: 'asdfasdf',
+  wca_id: '2005FLEI01',
+  delegate: true
+})
+berkeley_spring = Tournament.create({
+  name: "Berkeley Spring 2015",
+  organizer_id: dan.id,
+  delegate_id: jeremy.id,
+  location: "Berkeley, California, USA",
+  venue: "UC Berkeley",
+  start_date: Date.new(2015, 4, 12),
+  end_date: Date.new(2015, 4, 12)
+})
+us_nats = Tournament.create({
+  name: "US Nationals 2015",
+  organizer_id: ty.id,
+  delegate_id: ty.id,
+  location: "40 Folly Field Rd, Hilton Head Island, SC 29928",
+  venue: "hilton head resort",
+  start_date: Date.new(2015, 7, 31),
+  end_date: Date.new(2015, 8, 2),
+})
+sf = Tournament.create({
+  name: "SF Tournament 2015",
+  organizer_id: dan.id,
+  delegate_id: jeremy.id,
+  location: "The Warfield San Francisco, California, USA",
+  venue: "The Warfield",
+  start_date: Date.new(2015, 4, 30),
+  end_date: Date.new(2015, 4, 30)
+})
+wc = Tournament.create({
+  name: "World Championships 2015",
+  organizer_id: dan.id,
+  delegate_id: jeremy.id,
+  location: "Rua Vergueiro, 1987, Sao Paulo, Brazil",
+  venue: "ETAPA",
+  start_date: Date.new(2015, 7, 17),
+  end_date: Date.new(2015, 7, 19)
+})
