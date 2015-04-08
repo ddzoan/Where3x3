9401c87769f33431af9dad20f19dc73767f8f5fc
Dan Dzoan <dandzoan@gmail.com>
2015-04-07 22:26:01 -0700

    fix new user redirect to root, require login to view root page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login!(@user)
-      redirect_to user_url(@user)
+      redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
