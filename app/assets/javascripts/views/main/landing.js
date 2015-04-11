94240c3944303b8098dda8520f20ca8e67212683
Dan Dzoan <dandzoan@gmail.com>
2015-04-11 11:39:01 -0700

    autocomplete search bar for landing page

--- app/assets/javascripts/views/main/landing.js
+++ app/assets/javascripts/views/main/landing.js
@@ -4,15 +4,28 @@ Where3x3.Views.LandingPage = Backbone.CompositeView.extend({
   events: {
     'submit': 'submit'
   },
+
   render: function(){
     var content = this.template();
     this.$el.html(content);
+    this.attachAutocomplete();
     return this;
   },
+
+  attachAutocomplete: function(){
+    var locationBox = $('input#autocomplete')[0];
+    var autocomplete = new google.maps.places.Autocomplete(locationBox, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+      var place = autocomplete.getPlace();
+      $('input[name="lat"]').val(place.geometry.location.lat());
+      $('input[name="lng"]').val(place.geometry.location.lng());
+    });
+  },
+
   submit: function(event){
     event.preventDefault();
     var formData = this.$('form').serializeJSON();
-    if(formData.loc){
+    if(formData.loc && formData.lat === "" && formData.lng === ""){
       this.getLatLng(formData.loc, function(resp){
         var lat = resp.results[0].geometry.location.lat;
         var lng = resp.results[0].geometry.location.lng;
@@ -28,6 +41,8 @@ Where3x3.Views.LandingPage = Backbone.CompositeView.extend({
       var search_w_params = 'search?loc=' + formData.loc +
         '&start=' + formData.start +
         '&end=' + formData.end +
+        '&lat=' + formData.lat +
+        '&lng=' + formData.lng +
         '&rad=' + formData.rad;
       Backbone.history.navigate(search_w_params, { trigger: true });
     }
