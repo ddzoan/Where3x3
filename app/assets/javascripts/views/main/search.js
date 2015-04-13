c15efea6fd3c2901d9f3da68f5fb72ef3674627e
Dan Dzoan <dandzoan@gmail.com>
2015-04-12 18:23:21 -0700

    new fetch when new city entered

--- app/assets/javascripts/views/main/search.js
+++ app/assets/javascripts/views/main/search.js
@@ -4,25 +4,25 @@ Where3x3.Views.SearchPage = Backbone.CompositeView.extend({
 
   initialize: function(options){
     this.tournaments = this.collection;
-      delete options.search.loc;
-      this.tournaments.fetch({
-        data: {
-          search: options.search
-        }
-      });
+    this.loc = options.search.loc;
+    delete options.search.loc;
+    this.tournaments.fetch({
+      data: {
+        search: options.search
+      }
+    });
 
     var index = new Where3x3.Views.TournamentIndex({
       collection: this.tournaments
     });
     this.addSubview('#tournament-browse', index);
-
-    var loc = new Where3x3.Views.SearchBar({
-      loc: options.search.loc,
+    this.search = new Where3x3.Views.SearchBar({
+      loc: this.loc,
       rad: options.search.rad,
       start_date: options.search.start,
       end_date: options.search.end
     });
-    this.addSubview('.search', loc);
+    this.addSubview('.search', this.search);
 
     var lat = 37.781273; //default values if none specified
     var lng = -122.411463;
@@ -40,10 +40,38 @@ Where3x3.Views.SearchPage = Backbone.CompositeView.extend({
     var autocomplete = new google.maps.places.Autocomplete(locationBox, { types: ['geocode'] });
     google.maps.event.addListener(autocomplete, 'place_changed', function(){
       var place = autocomplete.getPlace();
-      console.log('do search');
-      var lat = place.geometry.location.lat();
-      var lng = place.geometry.location.lng();
-      this.map.centerMap(lat, lng);
+      var formData = this.search.$el.serializeJSON();
+
+      if(place.geometry){
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        this.map.centerMap(lat, lng);
+
+        this.loc = formData.loc;
+        delete formData.loc;
+        formData.lat = lat;
+        formData.lng = lng;
+
+        this.tournaments.fetch({
+          data: {
+            search: formData
+          }
+        });
+      } else {
+        // use geocode to search name
+
+        // this.getLatLng(place.name, function(resp){
+        //   var lat = resp.results[0].geometry.location.lat;
+        //   var lng = resp.results[0].geometry.location.lng;
+        //   var search_w_params = 'search?loc=' + formData.loc +
+        //     '&start=' + formData.start +
+        //     '&end=' + formData.end +
+        //     '&lat=' + lat +
+        //     '&lng=' + lng +
+        //     '&rad=' + formData.rad;
+        //   Backbone.history.navigate(search_w_params, { trigger: true });
+        // });
+      }
     }.bind(this));
   },
 
@@ -65,3 +93,5 @@ Where3x3.Views.SearchPage = Backbone.CompositeView.extend({
     this.map.remove();
   }
 });
+
+_.extend(Where3x3.Views.SearchPage.prototype, Where3x3.MapView);
