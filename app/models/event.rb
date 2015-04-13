2637157b0f87d1357781f37d4980404bfa4ce076
Dan Dzoan <dandzoan@gmail.com>
2015-04-13 15:23:03 -0700

    scrubbed wca_comp db, scripts to automate scrubbing

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,7 @@
 #
 
 class Event < ActiveRecord::Base
-  enum event_type: [:e3x3, :e4x4, :e5x5, :e2x2, :bld, :oh, :fm, :feet, :minx, :pyramix, :sq1, :clock, :skewb, :e6x6, :e7x7, :bld4, :bld5, :multibld]
+  enum event_type: [:e3x3, :e4x4, :e5x5, :e2x2, :bld, :oh, :fm, :feet, :minx, :pyramix, :sq1, :clock, :skewb, :e6x6, :e7x7, :bld4, :bld5, :multibld, :magic, :mmagic]
 
   validates :tournament_id, :event_type, presence: true
 
