4e2f6c69bfde1d611345bea4700727525a3b2147
Dan Dzoan <dandzoan@gmail.com>
2015-04-16 11:39:36 -0700

    remove registration form if past start date

--- app/assets/javascripts/views/tournaments/registrationForm.js
+++ app/assets/javascripts/views/tournaments/registrationForm.js
@@ -1,5 +1,6 @@
 Where3x3.Views.RegistrationForm = Backbone.CompositeView.extend({
   template: JST['tournaments/registration_form'],
+  overTemplate: JST['tournaments/registration_over'],
   tagName: 'form',
   events: {
     'submit': 'submit'
@@ -8,7 +9,12 @@ Where3x3.Views.RegistrationForm = Backbone.CompositeView.extend({
     this.listenTo(this.model, 'sync', this.render);
   },
   render: function(){
-    var content = this.template({ events: this.model.events, keys: this.keys() });
+    var content;
+    if(new Date(this.model.escape('start_date')) > new Date(Date.now())){
+      content = this.template({ events: this.model.events, keys: this.keys() });
+    } else {
+      content = this.overTemplate();
+    }
     this.$el.html(content);
     return this;
   },
