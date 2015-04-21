1f35d1a5b4d348159b384861f87c59aac852c3d6
Dan Dzoan <dandzoan@gmail.com>
2015-04-20 18:08:40 -0700

    fix #14

--- app/assets/javascripts/views/search/searchBar.js
+++ app/assets/javascripts/views/search/searchBar.js
@@ -1,7 +1,6 @@
 Where3x3.Views.SearchBar = Backbone.CompositeView.extend({
   template: JST['search/search_bar'],
   tagName: 'form',
-  className: 'form-inline',
 
   initialize: function(options){
     this.loc = options.loc;
