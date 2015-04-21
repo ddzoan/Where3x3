87acefcce885f423b28a41c76139b4c0c17727ed
Dan Dzoan <dandzoan@gmail.com>
2015-04-21 01:05:52 -0700

    show events in a smaller list with an expand link

--- app/assets/javascripts/views/tournaments/eventsList.js
+++ app/assets/javascripts/views/tournaments/eventsList.js
@@ -1,8 +1,18 @@
 Where3x3.Views.EventsList = Backbone.CompositeView.extend({
   template: JST['tournaments/events_list'],
+  events: {
+    'click .expand-events': 'showAll'
+  },
   initialize: function(){
     this.listenTo(this.model, 'sync', this.render);
   },
+
+  showAll: function(event){
+    console.log("showall event");
+    this.$('.some').css('display', 'none');
+    this.$('.all').show("slow");
+  },
+
   render: function(){
     var content = this.template({ events: this.model.events, events_hash: this.events_hash() });
     this.$el.html(content);
