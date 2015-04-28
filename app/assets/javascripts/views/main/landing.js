cca3566e87badaad50b454a3f61e0dbc5b959a0d
Dan Dzoan <dandzoan@gmail.com>
2015-04-27 21:04:21 -0700

    fix #9 prevent enter key from submitting when the dropdown list is visible, fix date offset

--- app/assets/javascripts/views/main/landing.js
+++ app/assets/javascripts/views/main/landing.js
@@ -13,7 +13,7 @@ Where3x3.Views.LandingPage = Backbone.CompositeView.extend({
       $('input[name="lat"]').val(place.geometry.location.lat());
       $('input[name="lng"]').val(place.geometry.location.lng());
     }.bind(this));
-    this.attachDatePickers({ offest: {years: 1} });
+    this.attachDatePickers({ offset: {years: 1} });
 
     this.$('#submit').tooltip('show');
     return this;
